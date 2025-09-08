7cda99f043c8c931c53d8929a3bdfd5e011b0a64
Juan Jose Carranza Garcia <juancarranza7561@gmail.com>
2025-09-07 19:29:32 -0500

    Ruff fixes

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+"""A simple FastAPI application."""
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,9 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """A root endpoint that returns a welcome message."""
     return {"message": "Hello World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
+    """An endpoint to read an item by its ID and an optional query parameter."""
     return {"item_id": item_id, "q": q}
