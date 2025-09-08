dd9e3babd298892f7da3655868d28494ad96b742
Juan Carranza <juancarranza7561@gmail.com>
2025-09-07 19:56:14 -0500

    Initial commit (#1)
    * First commit
    
    * Use uv
    
    * Install ruff
    
    * add ruff
    
    * Ruff fixes
    
    * Include action for ruff
    
    * Split linting and formating
    
    * Rename action
    
    * just for testing
    
    * Revert "just for testing"
    
    This reverts commit 2e6b147e5574186efb4f18bc631958f9ad558ccd.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # fast-api-docker
+
+## Development
+
+```shell
+uv run fastapi dev
+```
+
+## Dockerfile
+
+Build
+```shell
+docker build -t fast-api-docker .
+```
+
+Run
+```shell
+docker run -d -p 8000:80 fast-api-docker
+```
---
+++ app/main.py
@@ -0,0 +1,17 @@
+"""A simple FastAPI application."""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """A root endpoint that returns a welcome message."""
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    """An endpoint to read an item by its ID and an optional query parameter."""
+    return {"item_id": item_id, "q": q}
